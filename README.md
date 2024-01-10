48dfea5b329ac5c1f0c74865151947075f6ec3d3
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-01-09 17:32:42 -0800

    To pass betty

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a solo simple shell project done 
+By me, Adjocacher Abednego.
+
---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * void_sum - Entry point of the function sum
+ * @a: firt number
+ * @b: second number
+ * Return: sum
+ */
+
+void_sum(int a, int b)
+{
+	printf("This is to pass betty checks");
+	return (a + b);
+}
